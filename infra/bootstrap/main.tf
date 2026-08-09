e7ae7875b4fa79c83b667e945b63d9a86dd41841
Riccardo Ruspoli <riccardoruspoli@gmail.com>
2026-08-09 07:28:36 +0200

    refactor: swap DynamoDB with lockfile

--- infra/bootstrap/main.tf
+++ infra/bootstrap/main.tf
@@ -52,18 +52,3 @@ resource "aws_s3_bucket_public_access_block" "terraform_state" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
-
-resource "aws_dynamodb_table" "terraform_lock" {
-  name         = var.terraform_lock_table_name
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  lifecycle {
-    prevent_destroy = true
-  }
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
--- infra/bootstrap/variables.tf
+++ infra/bootstrap/variables.tf
@@ -20,8 +20,3 @@ variable "terraform_state_bucket_name" {
   type        = string
   description = "S3 bucket name for Terraform remote state."
 }
-
-variable "terraform_lock_table_name" {
-  type        = string
-  description = "DynamoDB table name for Terraform state locking."
-}
